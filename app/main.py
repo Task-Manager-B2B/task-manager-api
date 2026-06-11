ab67a4213cb35ab348d358552dfb688ec66765ad
Sarah Lopes <sasarinha13@gmail.com>
2026-06-11 10:03:53 -0300

    feat: estrutura inicial FastAPI

---
+++ app/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastApi(
+    title='Task B2B',
+    description='Task Manager B2B',
+    version='1.0.0'
+)
+
+@app.get('/Health')
+async def health():
+    return {'status': 'OK'}
